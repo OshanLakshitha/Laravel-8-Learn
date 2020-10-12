2b41069ef084cfb3b83e695a1fdedcfa7364b794
OshanLakshitha <56622283+OshanLakshitha@users.noreply.github.com>
2020-10-12 10:36:00 +0530

    routes test and routes contarain

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,32 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// (http://127.0.0.1:8000/api/users) call normal method calling
+/*Route::get('/users',function(){
+    return "Hi user";
+});*/
+
+//(http://127.0.0.1:8000/api/users/jone) parameter passing
+/*Route::get('/users/{name}',function($name){
+    return "Hi user" . $name;
+});
+*/
+
+//call null value
+/*Route::get('/users/{name?}',function($name=null){
+    return "Hi user" . $name;
+});*/
+
+//with out call numbers
+Route::get('/users/{name?}',function($name=null){
+    return "Hi user" . $name;
+})->where('name','[a-zA-Z]+');
+
+//only pass numbes parameters
+Route::get('/usersID/{id?}',function($id=null){
+    return "Hi user" . $id;
+})->where('id','[0-9]+');
