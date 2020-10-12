3e6fb97490b591a7e5aecaf1bf286be7740cbdca
OshanLakshitha <56622283+OshanLakshitha@users.noreply.github.com>
2020-10-12 10:51:27 +0530

    Restric request

--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,13 @@ Route::get('/users/{name?}',function($name=null){
 Route::get('/usersID/{id?}',function($id=null){
     return "Hi user" . $id;
 });
+
+//Restric get request and post request only 
+Route::match(['get','post'],'/students',function(Request $req){
+    return "Request methods is". $req->method();
+});
+
+// any request formate can reqested
+Route::any('/post',function(Request $req){
+    return "Request methods is". $req->method();
+});
