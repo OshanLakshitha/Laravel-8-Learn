530e7e2a5f630f9339efae4b499b2cf316dfba59
OshanLakshitha <56622283+OshanLakshitha@users.noreply.github.com>
2020-10-12 15:04:27 +0530

    pass array list using controller for componets

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,11 @@
 <div>
     <h1>this is header</h1>
     <h3>Hello {{$name}}</h3>
+    <h3>fruits are
+     <ul>
+         @foreach ($fruits as $fruit)
+            <li>{{$fruit}}</li>
+         @endforeach
+     </ul>   
+    </h3>
 </div>
