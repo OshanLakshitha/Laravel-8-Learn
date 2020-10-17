410982271d21be23fc83f890ee59b35ab8518403
OshanLakshitha <56622283+OshanLakshitha@users.noreply.github.com>
2020-10-17 12:10:04 +0530

    one data seeding data base first cmd run (composer dump-autoload) and secound cmd code (php artisan db:seed)

--- database/seeders/postTableSeeder.php
+++ database/seeders/postTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class postTableSeeder extends Seeder
 {
@@ -13,6 +14,9 @@ class postTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('posts')->insert([
+          'title'=>'First Post Title',
+          'body'=>'First Post Description'
+      ]);
     }
 }
